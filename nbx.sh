bfede72c81b55183b1fcc4c1d125b5190d3a7849
thomas tsai <thomas@biotrump.com>
2015-06-12 19:09:45 +0800

    +#!/bin/bash +#my PC +#ssh -X thomas@addr:192.168.9.42 +#danny's pc +ssh -X thomas@192.168.9.12 +#NB eth0 +#ssh -X thomas@192.168.9.85 +#NB wifi +#ssh -X thomas@192.168.10.38

--- nbx.sh
+++ nbx.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ssh -X thomas@192.168.2.38
+#my PC
+#ssh -X thomas@addr:192.168.9.42
+#danny's pc
+ssh -X thomas@192.168.9.12
+#NB eth0
+#ssh -X thomas@192.168.9.85
+#NB wifi
+#ssh -X thomas@192.168.10.38
